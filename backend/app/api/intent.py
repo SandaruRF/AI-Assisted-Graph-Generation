53451b6216df2482bb9fa79fdfa163cfbbe2ad91
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-04-17 15:19:25 +0530

    [AAGG-17] Remove api endpoint for intent classification

--- backend/app/api/intent.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from agents.intent_agent.intent_classifier import IntentClassifier
-
-router = APIRouter()
-classifier = IntentClassifier()
-
-@router.post("/classify-intent")
-async def classify_intent(query: str):
-    return classifier.classify(query)
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from agents.sql_agent.sql_query import router as query_router
 from agents.sql_agent.database_api import router as get_database_router
-from api.intent import router as intent_router
 from api.user_prompt import router as user_prompt_router
 
 app = FastAPI(
@@ -21,7 +20,6 @@ app.add_middleware(
 
 app.include_router(query_router, prefix="/sql", tags=["NL to SQL Query"])
 app.include_router(get_database_router, prefix="/sql", tags=["Database Connector"])
-app.include_router(intent_router, prefix="/intent", tags=["Intent Classification"])
 app.include_router(user_prompt_router, tags=["User Prompt"])
 
 @app.get("/")
