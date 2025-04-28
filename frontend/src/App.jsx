34dad6d842e4deb0ca9bfe01599a38b9cde6be8f
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-04-29 03:43:36 +0530

    [AAGG-19] Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ExistingDatabaseConnection from "./pages/ExistingDatabaseConnection";
-import DatabaseConnection from "./pages/DatabaseConnection"; // Default import
-import EditDatabaseConnection from "./pages/EditDatabaseConnection"; // Import the Edit component
+import DatabaseConnection from "./pages/DatabaseConnection";
+import EditDatabaseConnection from "./pages/EditDatabaseConnection";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Default Route */}
-        <Route path="/" element={<ExistingDatabaseConnection />} />
-        {/* Database Connection Routes */}
         <Route
           path="/existing-connections"
           element={<ExistingDatabaseConnection />}
         />
-        <Route path="/new-connection" element={<DatabaseConnection />} />{" "}
-        {/* Create New Connection */}
+        <Route path="/new-connection" element={<DatabaseConnection />} />
         <Route
           path="/edit-connection/:id"
           element={<EditDatabaseConnection />}
-        />{" "}
-        {/* Edit Connection by ID */}
+        />
       </Routes>
     </Router>
   );
