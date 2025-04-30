7d09cf0bde63fe775e8f03e171d4cadc4bff3ea8
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-04-30 22:13:38 +0530

    [AAGG-34] Remove duplications and clean code

--- frontend/src/components/graphs/BarChart.jsx
+++ frontend/src/components/graphs/BarChart.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Plot from "react-plotly.js";
 
-const type = "num_1_cat_1_temp_0";
-const mockData = [
+const type = "num_1_cat_2_temp_0";
+
+const mockData1 = [
   { Product: "Product A", Revenue: 12500 },
   { Product: "Product B", Revenue: 8700 },
   { Product: "Product C", Revenue: 15300 },
@@ -10,177 +11,100 @@ const mockData = [
   { Product: "Product E", Revenue: 13800 },
 ];
 
-// const type = "num_1_cat_2_temp_0";
-// const mockData = [
-//   { Product: "Laptop", Region: "North", Sales: 15000 },
-//   { Product: "Laptop", Region: "South", Sales: 12000 },
-//   { Product: "Laptop", Region: "East", Sales: 13500 },
-//   { Product: "Laptop", Region: "West", Sales: 11000 },
-//   { Product: "Laptop", Region: "Central", Sales: 12500 },
+const mockData2 = [
+  { Product: "Laptop", Region: "North", Sales: 15000 },
+  { Product: "Laptop", Region: "South", Sales: 12000 },
+  { Product: "Laptop", Region: "East", Sales: 13500 },
+  { Product: "Laptop", Region: "West", Sales: 11000 },
+  { Product: "Laptop", Region: "Central", Sales: 12500 },
+  { Product: "Phone", Region: "North", Sales: 10000 },
+  { Product: "Phone", Region: "South", Sales: 9000 },
+  { Product: "Phone", Region: "East", Sales: 9500 },
+  { Product: "Phone", Region: "West", Sales: 8700 },
+  { Product: "Phone", Region: "Central", Sales: 9100 },
+  { Product: "Tablet", Region: "North", Sales: 8000 },
+  { Product: "Tablet", Region: "South", Sales: 8500 },
+  { Product: "Tablet", Region: "East", Sales: 7800 },
+  { Product: "Tablet", Region: "West", Sales: 7200 },
+  { Product: "Tablet", Region: "Central", Sales: 7600 },
+];
 
-//   { Product: "Phone", Region: "North", Sales: 10000 },
-//   { Product: "Phone", Region: "South", Sales: 9000 },
-//   { Product: "Phone", Region: "East", Sales: 9500 },
-//   { Product: "Phone", Region: "West", Sales: 8700 },
-//   { Product: "Phone", Region: "Central", Sales: 9100 },
+const generateGroupedOrStackedData = (data, orientation = "v") => {
+  const cat1 = [...new Set(data.map((item) => Object.values(item)[0]))];
+  const cat2 = [...new Set(data.map((item) => Object.values(item)[1]))];
 
-//   { Product: "Tablet", Region: "North", Sales: 8000 },
-//   { Product: "Tablet", Region: "South", Sales: 8500 },
-//   { Product: "Tablet", Region: "East", Sales: 7800 },
-//   { Product: "Tablet", Region: "West", Sales: 7200 },
-//   { Product: "Tablet", Region: "Central", Sales: 7600 },
-// ];
+  return cat2.map((cat_2_val) => ({
+    [orientation === "v" ? "x" : "y"]: cat1,
+    [orientation === "v" ? "y" : "x"]: cat1.map((cat_1_val) => {
+      const entry = data.find(
+        (item) =>
+          Object.values(item)[0] === cat_1_val &&
+          Object.values(item)[1] === cat_2_val
+      );
+      return entry ? Object.values(entry)[2] : 0;
+    }),
+    name: cat_2_val,
+    type: "bar",
+    orientation: orientation === "h" ? "h" : undefined,
+  }));
+};
 
 const BarChart = () => {
-  let data = [];
-  let data_1 = [];
-  let data_2 = [];
-  let data_3 = [];
-  let barmode = "";
-  let barmode_1 = "";
-  let barmode_2 = "";
-  let barmode_3 = "";
-  let title = "";
-  let title_1 = "";
-  let title_2 = "";
-  let title_3 = "";
+  const charts = [];
 
   if (type === "num_1_cat_1_temp_0") {
-    title = "Basic Bar Chart";
-    data = [
-      {
-        x: mockData.map((item) => Object.values(item)[0]),
-        y: mockData.map((item) => Object.values(item)[1]),
-        type: "bar",
-      },
-    ];
+    const x = mockData1.map((item) => Object.values(item)[0]);
+    const y = mockData1.map((item) => Object.values(item)[1]);
 
-    title_1 = "Horizontal Basic Bar Chart";
-    data_1 = [
-      {
-        x: mockData.map((item) => Object.values(item)[1]),
-        y: mockData.map((item) => Object.values(item)[0]),
-        type: "bar",
-        orientation: "h",
-      },
-    ];
+    charts.push({
+      title: "Basic Bar Chart",
+      data: [{ x, y, type: "bar" }],
+    });
+
+    charts.push({
+      title: "Horizontal Basic Bar Chart",
+      data: [{ x: y, y: x, type: "bar", orientation: "h" }],
+    });
   } else if (type === "num_1_cat_2_temp_0") {
-    const cat_1 = [...new Set(mockData.map((item) => Object.values(item)[0]))];
-    const cat_2 = [...new Set(mockData.map((item) => Object.values(item)[1]))];
-
-    title = "Grouped Bar Chart";
-    barmode = "group";
-    data = cat_2.map((cat_2_val) => {
-      return {
-        x: cat_1,
-        y: cat_1.map((cat_1_val) => {
-          const foundItem = mockData.find(
-            (item) =>
-              Object.values(item)[0] === cat_1_val &&
-              Object.values(item)[1] === cat_2_val
-          );
-          return foundItem ? Object.values(foundItem)[2] : 0;
-        }),
-        name: cat_2_val,
-        type: "bar",
-      };
+    charts.push({
+      title: "Grouped Bar Chart",
+      data: generateGroupedOrStackedData(mockData2, "v"),
+      barmode: "group",
     });
 
-    title_1 = "Stacked Bar Chart";
-    barmode_1 = "stack";
-    data_1 = cat_2.map((cat_2_val) => {
-      return {
-        x: cat_1,
-        y: cat_1.map((cat_1_val) => {
-          const foundItem = mockData.find(
-            (item) =>
-              Object.values(item)[0] === cat_1_val &&
-              Object.values(item)[1] === cat_2_val
-          );
-          return foundItem ? Object.values(foundItem)[2] : 0;
-        }),
-        name: cat_2_val,
-        type: "bar",
-      };
+    charts.push({
+      title: "Stacked Bar Chart",
+      data: generateGroupedOrStackedData(mockData2, "v"),
+      barmode: "stack",
     });
 
-    title_2 = "Horizontal Stacked Bar Chart";
-    barmode_2 = "stack";
-    data_2 = cat_2.map((cat_2_val) => {
-      return {
-        y: cat_1,
-        x: cat_1.map((cat_1_val) => {
-          const foundItem = mockData.find(
-            (item) =>
-              Object.values(item)[0] === cat_1_val &&
-              Object.values(item)[1] === cat_2_val
-          );
-          return foundItem ? Object.values(foundItem)[2] : 0;
-        }),
-        name: cat_2_val,
-        type: "bar",
-        orientation: "h",
-      };
+    charts.push({
+      title: "Horizontal Stacked Bar Chart",
+      data: generateGroupedOrStackedData(mockData2, "h"),
+      barmode: "stack",
     });
 
-    title_3 = "Horizontal Grouped Bar Chart";
-    barmode_3 = "group";
-    data_3 = cat_2.map((cat_2_val) => {
-      return {
-        y: cat_1,
-        x: cat_1.map((cat_1_val) => {
-          const foundItem = mockData.find(
-            (item) =>
-              Object.values(item)[0] === cat_1_val &&
-              Object.values(item)[1] === cat_2_val
-          );
-          return foundItem ? Object.values(foundItem)[2] : 0;
-        }),
-        name: cat_2_val,
-        type: "bar",
-        orientation: "h",
-      };
+    charts.push({
+      title: "Horizontal Grouped Bar Chart",
+      data: generateGroupedOrStackedData(mockData2, "h"),
+      barmode: "group",
     });
   }
 
   return (
     <div>
-      <Plot
-        data={data}
-        layout={{
-          width: 640,
-          height: 480,
-          title: { text: title },
-          barmode: barmode,
-        }}
-      />
-      <Plot
-        data={data_1}
-        layout={{
-          width: 640,
-          height: 480,
-          title: { text: title_1 },
-          barmode: barmode_1,
-        }}
-      />
-      <Plot
-        data={data_2}
-        layout={{
-          width: 640,
-          height: 480,
-          title: { text: title_2 },
-          barmode: barmode_2,
-        }}
-      />
-      <Plot
-        data={data_3}
-        layout={{
-          width: 640,
-          height: 480,
-          title: { text: title_3 },
-          barmode: barmode_3,
-        }}
-      />
+      {charts.map((chart, index) => (
+        <Plot
+          key={index}
+          data={chart.data}
+          layout={{
+            width: 640,
+            height: 480,
+            title: { text: chart.title },
+            barmode: chart.barmode,
+          }}
+        />
+      ))}
     </div>
   );
 };
