5f559318c5dbf949e15a26ff32cc03a762fce151
SithuminiAbeysekara <sudeepaabeysekara2002@gmail.com>
2025-05-02 17:34:54 +0530

    Line chrts- Simple Univariate Time Series

--- frontend/src/components/graphs/LineChart.jsx
+++ frontend/src/components/graphs/LineChart.jsx
@@ -1,84 +1,67 @@
 import React from "react";
 import Plot from "react-plotly.js";
 
-/*const type = "num_1_cat_1_temp_1"; 
-const Data = [
-  { month: "2024-01", city: "Colombo", temperature: 28 },
-  { month: "2024-01", city: "Kandy", temperature: 22 },
-  { month: "2024-02", city: "Colombo", temperature: 29 },
-  { month: "2024-02", city: "Kandy", temperature: 23 },
-  { month: "2024-03", city: "Colombo", temperature: 31 },
-  { month: "2024-03", city: "Kandy", temperature: 25 }
+const mockData1 = [
+  { "Month": "2025-01-01", "Temperature": 4.2 },
+  { "Month": "2025-02-01", "Temperature": 5.0 },
+  { "Month": "2025-03-01", "Temperature": 9.3 },
+  { "Month": "2025-04-01", "Temperature": 14.8 },
+  { "Month": "2025-05-01", "Temperature": 19.6 },
+  { "Month": "2025-06-01", "Temperature": 24.1 },
+  { "Month": "2025-07-01", "Temperature": 27.3 },
+  { "Month": "2025-08-01", "Temperature": 26.8 },
+  { "Month": "2025-09-01", "Temperature": 22.5 },
+  { "Month": "2025-10-01", "Temperature": 16.0 },
+  { "Month": "2025-11-01", "Temperature": 9.4 },
+  { "Month": "2025-12-01", "Temperature": 5.1 }
 ]
-;*/
+;
 
-const type = "num_1_cat_0_temp_1"; 
-
-const Data = [
-  { time: "2024-01", rainfall: 78 },
-  { time: "2024-02", rainfall: 65 },
-  { time: "2024-03", rainfall: 89 },
-  { time: "2024-04", rainfall: 102 },
-  { time: "2024-05", rainfall: 75 }
-];
+const type = "num_1_cat_0_temp_1";
+const mockData = mockData1;
 
 const LineChart = () => {
-  const sample = Data[0];
-  const keys = Object.keys(sample);
+  const charts = [];
 
-  const isTimeString = (val) =>
-    typeof val === "string" && /^\d{4}-\d{2}(-\d{2})?$/.test(val);
+  if (type === "num_1_cat_0_temp_1") {
+    const [xKey, yKey] = Object.keys(mockData[0]);
+    const x = mockData.map((item) => Object.values(item)[0]);
+    const y = mockData.map((item) => Object.values(item)[1]);
 
-  const isNumeric = (val) => typeof val === "number";
-
-  let timeKey, categoryKey, valueKey;
-  for (let key of keys) {
-    const val = sample[key];
-    if (!timeKey && isTimeString(val)) timeKey = key;
-    else if (!valueKey && isNumeric(val)) valueKey = key;
-  }
-
-  categoryKey = keys.find((k) => k !== timeKey && k !== valueKey);
-
-  const layout = {
-    title: "Dynamic Time Series Line Chart",
-    xaxis: { title: timeKey, type: "date" },
-    yaxis: { title: valueKey },
-    width: 700,
-    height: 500,
-  };
-
-  let data = [];
-
-  if (type === "num_1_cat_1_temp_1") {
-    const categories = [...new Set(Data.map((item) => item[categoryKey]))];
-
-    data = categories.map((cat) => {
-      const filtered = Data.filter((d) => d[categoryKey] === cat);
-      return {
-        x: filtered.map((d) => normalizeDate(d[timeKey])),
-        y: filtered.map((d) => d[valueKey]),
-        type: "scatter",
-        mode: "lines+markers",
-        name: cat,
-      };
+    charts.push({
+      title: "Simple Univariate Time Series",
+      xAxisTitle: xKey,
+      yAxisTitle: yKey,
+      data: [{ x, y, type: "Time", mode: "lines" }],
     });
-  } else if (type === "num_1_cat_0_temp_1") {
-    data = [
-      {
-        x: Data.map((d) => normalizeDate(d[timeKey])),
-        y: Data.map((d) => d[valueKey]),
-        type: "scatter",
-        mode: "lines+markers",
-        name: "Value",
-      },
-    ];
   }
 
-  return <Plot data={data} layout={layout} />;
+  return (
+    <div>
+      {charts.map((chart, index) => (
+        <Plot
+          key={index}
+          data={chart.data}
+          layout={{
+            width: 640,
+            height: 480,
+            title: { text: chart.title },
+            xaxis: {
+              title: {
+                text: chart.xAxisTitle,
+              },
+              type: "date",
+            },
+            yaxis: {
+              title: {
+                text: chart.yAxisTitle,
+              },
+            },
+          }}
+        />
+      ))}
+    </div>
+  );
 };
-
-const normalizeDate = (val) =>
-  /^\d{4}-\d{2}$/.test(val) ? `${val}-01` : val;
 
 export default LineChart;
