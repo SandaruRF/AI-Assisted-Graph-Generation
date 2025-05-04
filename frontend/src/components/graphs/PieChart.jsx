68c7bff3721c0a2a5871bf1b6a42d2784f7c7cd1
Sandaru Fernando <sandarufernando2002@gmail.com>
2025-05-04 16:40:05 +0530

    [AAGG-34] Implement donut chart and pie chart sub plots

--- frontend/src/components/graphs/PieChart.jsx
+++ frontend/src/components/graphs/PieChart.jsx
@@ -26,15 +26,18 @@ const mockData2 = [
 
 // num_1_cat_1_temp_0  --  mockData1
 // num_1_cat_2_temp_0  --  mockData2
-const type = "num_1_cat_1_temp_0";
-const mockData = mockData1;
+const type = "num_1_cat_2_temp_0";
+const mockData = mockData2;
 
 const PieChart = () => {
   if (!mockData || mockData.length === 0) return null;
 
-  const opacity = 0.7;
-  let data = [];
+  const charts = [];
+  const opacity = 0.9;
+  let data,
+    data1 = [];
   let title,
+    title1,
     numericKey,
     categoryKey,
     categoryKey1 = "";
@@ -42,33 +45,43 @@ const PieChart = () => {
   if (type === "num_1_cat_1_temp_0") {
     [numericKey, categoryKey] = Object.keys(mockData[0]);
 
-    data = [
-      {
-        values: mockData.map((item) => item[numericKey]),
-        labels: mockData.map((item) => item[categoryKey]),
-        type: "pie",
-        opacity: opacity,
-        showlegend: true,
-        legendgrouptitle: { text: categoryKey },
-        hoverinfo: "text",
-        hovertext: mockData.map(
-          (item) =>
-            `${categoryKey}: ${item[categoryKey]}<br>${numericKey}: ${item[numericKey]}`
-        ),
-        text: mockData.map((item) => `${numericKey}: ${item[numericKey]}`),
-      },
-    ];
-    title = "Basic Pie Chart";
+    const createPieTrace = (hole, title) => ({
+      data: [
+        {
+          values: mockData.map((item) => item[numericKey]),
+          labels: mockData.map((item) => item[categoryKey]),
+          type: "pie",
+          opacity: opacity,
+          hole: hole,
+          showlegend: true,
+          legendgrouptitle: { text: categoryKey },
+          hoverinfo: "text",
+          hovertext: mockData.map(
+            (item) =>
+              `${categoryKey}: ${item[categoryKey]}<br>${numericKey}: ${item[numericKey]}`
+          ),
+          text: mockData.map((item) => `${numericKey}: ${item[numericKey]}`),
+        },
+      ],
+      title: title,
+    });
+
+    charts.push(createPieTrace(0.0, "Basic Pie Chart"));
+    charts.push(createPieTrace(0.4, "Donut Chart"));
   } else if (type === "num_1_cat_2_temp_0") {
     [numericKey, categoryKey, categoryKey1] = Object.keys(mockData[0]);
-    const categories = [...new Set(mockData.map((item) => item[categoryKey]))];
-    const categories1 = [
-      ...new Set(mockData.map((item) => item[categoryKey1])),
-    ];
+    let categories = [...new Set(mockData.map((item) => item[categoryKey]))];
+    let categories1 = [...new Set(mockData.map((item) => item[categoryKey1]))];
+
+    // categories - keys of category which has less cardinality
+    // categories1 - keys of category which has high cardinality
     if (categories.length > categories1.length) {
       let temp = categoryKey;
+      let temp1 = categories;
       categoryKey = categoryKey1;
       categoryKey1 = temp;
+      categories = categories1;
+      categories1 = temp1;
     }
 
     const labels = [];
@@ -138,15 +151,93 @@ const PieChart = () => {
       },
     ];
     title = "Grouped Pie Chart (Sunburst)";
+
+    charts.push({
+      data: data,
+      title: title,
+    });
+
+    /////////////////////////////////////////////////////////////
+
+    const category1Groups = {};
+    mockData.forEach((item) => {
+      if (!category1Groups[item[categoryKey1]]) {
+        category1Groups[item[categoryKey1]] = [];
+      }
+      category1Groups[item[categoryKey1]].push(item);
+    });
+    const nCols = 2;
+    const nRows = Math.ceil(categories1.length / nCols);
+
+    const annotations = categories1.map((category1, i) => {
+      const j = i < 2 ? 0 : 1;
+      console.log(category1);
+      return {
+        text: category1,
+        showarrow: false,
+        font: { size: 14 },
+        // x: i * 1 + 1,
+        // y: i * 1 + 0.5,
+        x: 0.18 + 0.63 * (i % 2),
+        y: 0.51 - 0.55 * (j % 2),
+        xref: "paper",
+        yref: "paper",
+        align: "center",
+      };
+    });
+
+    const createSubPieTrace = (hole, title) => ({
+      data: categories1.map((category1, i) => {
+        const group = category1Groups[category1];
+        const labels = group.map((item) => item[categoryKey]);
+        const values = group.map((item) => item[numericKey]);
+
+        return {
+          values: values,
+          labels: labels,
+          type: "pie",
+          opacity: opacity,
+          hole: hole,
+          name: category1,
+          domain: {
+            row: Math.floor(i / nCols),
+            column: i % nCols,
+          },
+          title: category1,
+          showlegend: true,
+          legendgrouptitle: { text: categoryKey },
+          hoverinfo: "label+percent+name+text",
+          text: values.map((val) => `${numericKey}: ${val}`),
+        };
+      }),
+      title: title,
+      height: 320 * nRows,
+      rows: nRows,
+      columns: nCols,
+      annotations: annotations,
+    });
+
+    charts.push(createSubPieTrace(0.0, "Pie Chart Sub Plots"));
+    charts.push(createSubPieTrace(0.4, "Donut Chart Sub Plots"));
   }
 
-  const layout = {
-    width: 640,
-    height: 480,
-    title: { text: title },
-  };
-
-  return <Plot data={data} layout={layout} />;
+  return (
+    <div>
+      {charts.map((chart, index) => (
+        <Plot
+          key={index}
+          data={chart.data}
+          layout={{
+            width: 640, // Adjust width based on number of columns
+            height: chart.height || 480,
+            title: { text: chart.title },
+            grid: { rows: chart.rows || 1, columns: chart.columns || 1 },
+            annotations: chart.annotations,
+          }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default PieChart;
